5242892dc5ebc1775845fbff794886f5d5557de4
jonixis <joni@stahlfamily.ch>
2022-04-01 19:48:38 +0200

    Upgrade all packages

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,10 +1,8 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
-const ip = require('internal-ip')
 const portFinderSync = require('portfinder-sync')
 
-const infoColor = (_message) =>
-{
+const infoColor = (_message) => {
     return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`
 }
 
@@ -14,24 +12,22 @@ module.exports = merge(
         mode: 'development',
         devServer:
         {
-            host: '0.0.0.0',
+            host: 'localhost',
             port: portFinderSync.getPort(8080),
-            contentBase: './dist',
-            watchContentBase: true,
+            static: {
+                directory: './dist',
+                watch: true
+            },
             open: true,
             https: false,
-            useLocalIp: true,
-            disableHostCheck: true,
-            overlay: true,
-            noInfo: true,
-            after: function(app, server, compiler)
-            {
-                const port = server.options.port
-                const https = server.options.https ? 's' : ''
-                const localIp = ip.v4.sync()
+            allowedHosts: 'all',
+            onAfterSetupMiddleware: function (devServer) {
+                const port = devServer.options.port
+                const https = devServer.options.https ? 's' : ''
+                const localIp = '127.0.0.1'
                 const domain1 = `http${https}://${localIp}:${port}`
                 const domain2 = `http${https}://localhost:${port}`
-                
+
                 console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
             }
         }
